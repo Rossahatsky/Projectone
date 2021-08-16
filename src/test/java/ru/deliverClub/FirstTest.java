9e9671b2ee9f1e2032cbe5e90257bdb1acbc7c2d
rossakhatskii <rossakhatskii@greendatasoft.ru>
2021-08-17 02:17:15 +0500

    Старт в автотестах.

---
+++ src/test/java/ru/deliverClub/FirstTest.java
@@ -0,0 +1,16 @@
+package ru.deliverClub;
+
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest {
+
+  @Test
+  public void firstTest() {
+      System.setProperty(
+      )
+      ChromeDriver driver = new ChromeDriver();
+      driver.get("https://www.delivery-club.ru");
+
+  }
+}
