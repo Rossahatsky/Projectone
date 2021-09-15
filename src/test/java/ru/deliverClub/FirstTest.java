3005fd79d1cee4520301bf429ff90df02aa260cc
rossakhatskii <rossakhatskii@greendatasoft.ru>
2021-09-15 23:31:00 +0500

    before and after method included

--- src/test/java/ru/deliverClub/FirstTest.java
+++ src/test/java/ru/deliverClub/FirstTest.java
@@ -1,16 +1,50 @@
 package ru.deliverClub;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstTest {
 
-  @Test
-  public void firstTest() {
-      System.setProperty(
-      )
-      ChromeDriver driver = new ChromeDriver();
-      driver.get("https://www.delivery-club.ru");
+    public ChromeDriver driver;
 
-  }
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+        ChromeDriver driver = new ChromeDriver();
+        System.out.println("test start");
+
+
+    }
+
+    @Test
+    public void firstTest() {
+
+
+        driver.get("https://www.delivery-club.ru");
+        String title = driver.getTitle();
+        Assert.assertTrue(title.equals("Delivery Club — Доставка еды и продуктов"));
+
+    }
+
+    @Test
+    public void firstTest() {
+
+
+        driver.get("https://www.delivery-club.ru");
+        String title = driver.getTitle();
+        Assert.assertTrue(title.equals("Delivery Club — Доставка еды и продуктов"));
+
+
+
+    }
+
+    @After
+    public void close() {
+        driver.quit();
+        System.out.println("test close");
+
+    }
 }
